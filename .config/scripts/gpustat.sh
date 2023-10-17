429a0c4ad03ed39c99423378eb468ba1db936473
Myo Thiha Kyaw <111453672+myothk@users.noreply.github.com>
2023-10-17 17:12:13 +0630

    show gpu stat

---
+++ .config/scripts/gpustat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#CPU
+
+
+
+#GPU
+FILE=/sys/bus/pci/devices/0000\:01\:00.0/power/runtime_status
+if  test -f "$FILE"; then
+	case $(cat $FILE) in
+		"suspended") GPU="off";;
+		"resuming") GPU="on!";;
+		"suspending") GPU="off!";;
+		"active") GPU="on";;
+	esac
+fi
+echo "GPU: $GPU"
